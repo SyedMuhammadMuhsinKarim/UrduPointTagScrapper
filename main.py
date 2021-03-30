5957f3965d54bec678af6e48c8e24a428e7c0204
Syed Muhammad Muhsin Karim <syedmohsinkarim@gmail.com>
2021-03-30 21:16:31 +0000

    Time Extraction module

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
-# Official Modules
-from bs4 import BeautifulSoup as bs
-import lxml
-import asyncio
-import json
 import time
-import re
 from pandas import DataFrame
-
-# My Modules
 from parallel_requests import calling_event
 from gzip_to_xml import gzip_to_xml
 from get_elements import Get_Elements
 from link_cleaner import post_link_cleaning
+from time_extaractors import post_time_extraction
 
-dd = "04"
+dd = "02"
 yyyy = "2021"
 mm = "03"
 
@@ -23,8 +16,6 @@ start_event = time.time() # For Debbugg
 
 # Open gZip content and get XML content
 content_xml = gzip_to_xml(url)
-
-# Links Cleanings
 news_links = post_link_cleaning(content_xml)
 
 # Get Contents by aSyncIO
@@ -39,17 +30,13 @@ categories,categories_links = Get_Elements(content_posts).get_category()
 
 # Get Times of the posts
 try: 
-  timefinder_reg = re.compile("[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\+[0-9]{4}")
-  news_time = [re.findall(timefinder_reg, json.dumps(elem.find_all("script", attrs={'type': 'application/ld+json'})[3].contents))[0] for elem in content_posts]
+  news_time = post_time_extraction(content_posts)
   df = DataFrame([headlines, categories, news_time], index=['headlines', 'categories', 'time']).T
   print(df) # For Debbugg
-except Exception as e:
-  print(e) # For Debbugg
+except:
   df = DataFrame([headlines, categories], index=['headlines', 'categories',]).T
   print(df) # For Debbugg
 
-print(url.split("/")[7].split(".")[0].split("-")) # For Debbugg
 df.to_csv(f'NewsData/{2021}/{mm}/{yyyy}-{mm}-{dd}.csv', encoding='utf-8-sig')
 
-end_event = time.time() # For Debbugg
-print(end_event - start_event) # For Debbugg
+print(time.time() - start_event) # For Debbugg
---
+++ time_extaractors.py
@@ -0,0 +1,15 @@
+import re
+import json
+
+def post_time_extraction(content_posts):
+    timefinder_reg = re.compile(
+        "[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\+[0-9]{4}")
+    return [
+        re.findall(
+            timefinder_reg,
+            json.dumps(
+                elem.find_all("script",
+                              attrs={'type':
+                                     'application/ld+json'})[3].contents))[0]
+        for elem in content_posts
+    ]
